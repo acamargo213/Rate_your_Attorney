5e348996041f60b09131dfccb393df314c548be2
Alejandra Camargo <acam.2134@gmail.com>
2017-07-12 23:33:20 +0000

    FixedSeeds

--- db/migrate/20170702021929_create_law_offices.rb
+++ db/migrate/20170702021929_create_law_offices.rb
@@ -3,7 +3,7 @@ class CreateLawOffices < ActiveRecord::Migration[5.0]
     create_table :law_offices do |t|
       t.string :law_office
       t.string :contact_information
-      add_attachment :picture
+      ## add_attachment :picture
       t.string :website_link
       ## t.string :practice_areas
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-## law_office = Law_office.create([{ law_offce: 'Camargo and Associates'}, {contact_information: 'Office Number: ,  Address: 00 Main St, Durham, NC 27701 '}, {website_link: 'www.camargoandassociates.com'}])
-## Law_office.create([{law_office: 'Cruzaley Law Firm'}, {contact information: 'Office number: , Address: 01 Main St, Durham, NC 27701'}, {website_link: 'www.cruzaleylawfirm.com'}])
-## Law_office.create([{law_office: 'Aaron Jimenez Immigration Law Office'}, {contact information: 'Office Number: , Address: 02 Main St, Durham, NC 27701}, {website_link: 'www.jimenezimmigration.com'}]) 
-## Law_office.create([{law_office: 'Maldonado Immigration'}, {contact information: 'Office Number: , Address: 03 Main St, Durham, NC 27701'}, {website_link: 'www.maldonadoimmgration.com'}])
-## Law_office.create([{law_office: 'Antonio Camargo Immigration'}, {contact information: 'Office Number: , Address: 04 Main St, Durham, NC 27701'}, {website_link:'www.antonioimmigration.com'}])
+Law_office = LawOffice.create(
+    
+      law_office: 'Camargo and Associates',
+      contact_information: 'Office Number: 123-4567 , Address: 00 Main St, Durham, NC 27701 ', 
+      website_link: 'www.camargoandassociates.com'
+    
+)
+
+LawOffice.create(
+  
+    law_office: 'Cruzaley Law Firm', 
+    contact_information: 'Office number: , Address: 01 Main St, Durham, NC 27701', 
+    website_link: 'www.cruzaleylawfirm.com'
+  
+)
+
+LawOffice.create(
+  
+    law_office: 'Aaron Jimenez Immigration Law Office',
+    contact_information: 'Office Number: 919-123-5678 , Address: 02 Main St, Durham, NC 27701',
+    website_link: 'www.jimenezimmigration.com'
+)
+
+LawOffice.create(
+  
+  law_office: 'Maldonado Immigration',
+  contact_information: 'Office Number: 919-213-3456 , Address: 03 Main St, Durham, NC 27701',
+  website_link: 'www.maldonadoimmgration.com'
+)
+
+LawOffice.create(
+  law_office: 'Antonio Camargo Immigration', 
+  contact_information: 'Office Number: 919-758-4784 , Address: 04 Main St, Durham, NC 27701', 
+  website_link:'www.antonioimmigration.com'
+)
