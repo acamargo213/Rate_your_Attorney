f939d3ef52f56b6cc5c07cecf54db3b3cbf6f63e
Alejandra Camargo <acam.2134@gmail.com>
2017-08-03 16:06:28 +0000

    UpdatedViewsNeedToFixSomething

--- app/controllers/law_offices_controller.rb
+++ app/controllers/law_offices_controller.rb
@@ -3,14 +3,25 @@ class LawOfficesController < ApplicationController
   end
 
   def new
-    @law_office = Law_office.new
+    @law_office = LawOffice.new
   end
   
   def create
+    @law_office = LawOffice.new
+    @law_office.name = params[:law_office][:name]
+    @law_office.phone_number = params[:law_office][:phone_number]
+    @law_office.street_address = params[:law_office][:street_address]
+    @law_office.city = params[:law_office][:city]
+    @law_office.state = params[:law_office][:state]
+    @law_office.zip = params[:law_office][:zip]
+    @law_office.website_link = params[:law_office][:website_link]
+    @law_office.save
+    
+    redirect_to '/'
   end
   
   def edit
-    @law_office = law_office.find(params[:id])
+    @law_office= LawOffice.find(params[:id])
   end
   
   def update
