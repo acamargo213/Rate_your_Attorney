0eac900012f181f07cb75e879a02094f678e4253
Alejandra Camargo <acam.2134@gmail.com>
2017-09-27 23:46:33 +0000

    Issues with sign up

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'devise'
 
 gem "letter_opener", :group => :development
 
+gem 'pry-nav'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :law_offices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'law_offices#index'
+  root to: 'law_offices#index'
   resources :law_offices do 
     collection do 
       get :search
