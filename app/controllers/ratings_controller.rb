1dd6450457b694acd23ec3e4b6772fc8cd80309e
Diana Cortes <dianapatricio2001@gmail.com>
2017-10-24 21:14:13 +0000

    rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,20 +1,17 @@
 class RatingsController < ApplicationController
   def create
-    #@rating= .new
-    #@rating=
+    @rating = Rating.new
+    @rating.comments = params[:rating][:comments]
+    @rating.stars = params[:rating][:stars]
+    @rating.law_office_id = params[:rating][:law_office_id]
+    @rating.save
     
-    
-    
-    
-    
-    
-    
-    
-    
-    #redirect_to @rating.law_office
+    redirect_to @rating.law_office
   end
-
-  
+    
+  def show
+    @rating = Rating.new
+  end
   
   
 end
