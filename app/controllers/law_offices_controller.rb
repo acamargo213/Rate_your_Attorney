8807fe2413b52f5f176d7dc8511b59dfa97b1e63
Andrea Angott <andrea.angott@scimedsolutions.com>
2017-12-05 17:33:08 -0500

    Bug fix

--- app/controllers/law_offices_controller.rb
+++ app/controllers/law_offices_controller.rb
@@ -11,7 +11,7 @@ class LawOfficesController < ApplicationController
            )
         )
       )
-    )
+    end
     @results.uniq!
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'law_offices#index'
+  root 'law_offices#search'
   resources :ratings
-  resources :law_offices do 
-    collection do 
+  resources :law_offices do
+    collection do
       get :search
     end
   end
