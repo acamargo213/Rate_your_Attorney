d081efb5ba1e2d5c62e0062bdc0a54a19580b69c
Alejandra Camargo <acam.2134@gmail.com>
2018-01-09 18:17:44 -0500

    'cssstylesheet'

--- app/controllers/law_offices_controller.rb
+++ app/controllers/law_offices_controller.rb
@@ -2,7 +2,7 @@ class LawOfficesController < ApplicationController
   def index
     @search = params[:search]
       @search = '' if @search == nil
-      @tokens = @search.split(",")
+      @tokens = @search.split
     @results = [ ]
     @tokens.each do |token|
       @results += LawOffice.where("name LIKE ?","%#{token}%").or(
